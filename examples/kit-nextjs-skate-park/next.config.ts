bbba194e26e43067f7013114ca28c0b4b0a31780
Thuan Nguyen <thuan.nguyen@concentrix.com>
2026-04-06 17:19:18 +0700

    4444

--- examples/kit-nextjs-skate-park/next.config.ts
+++ examples/kit-nextjs-skate-park/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
