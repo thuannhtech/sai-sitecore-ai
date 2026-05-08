c8243fc58886191c1852e4d1b5b09edf0b6039c3
Thuan Nguyen <thuan.nguyen@concentrix.com>
2026-05-08 13:23:43 +0700

    4444

--- examples/kit-nextjs-skate-park/src/lib/sitecore-client.ts
+++ examples/kit-nextjs-skate-park/src/lib/sitecore-client.ts
@@ -1,5 +1,8 @@
+import { enableDebug } from '@sitecore-content-sdk/core';
 import { SitecoreClient } from '@sitecore-content-sdk/nextjs/client';
 import scConfig from 'sitecore.config';
+
+enableDebug('content-sdk:http');
 
 const client = new SitecoreClient({
   ...scConfig,
